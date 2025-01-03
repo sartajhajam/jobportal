be1b0e1a0a749d088f081cb609d802bc43875e5b
sartaj <hajamsaj@gmail.com>
2025-01-03 15:25:44 +0530

    Front end files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/components_lite/Navbar";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar> 
+    </div>
+  )
+}
+
+export default App
